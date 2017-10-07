1ec1c4d4bd71cbb17d1189b5c7810d79183c7f3f
aniemiec758 <32038608+aniemiec758@users.noreply.github.com>
2017-10-07 15:51:53 -0500

    bow sprite now updates based on how many arrows are being shot, also changed cooldown times; probably will start on gyroscope stuff next before working on writing it all to a text file (and connecting it to the flash game)

--- app/src/main/java/com/example/aniem/madhacks/Bow.java
+++ app/src/main/java/com/example/aniem/madhacks/Bow.java
@@ -16,6 +16,8 @@ public class Bow extends AppCompatActivity {
 
     private ProgressBar progBow;
     private View bow;
+    private View bowtwo;
+    private View bowthree;
     private Button button;
 
     // 1x, 2x, 3x images
@@ -23,33 +25,26 @@ public class Bow extends AppCompatActivity {
     private View twored; private View twogreen;
     private View threered; private View threegreen;
 
-    // globar variables
-    private int numArrows; // number of arrows being shot at once
-    private int duration; // cooldown timer duration
-    private int upProg; // what the progressBar increments by as it cools down
+    // global variables
+    private int numArrows = 1; // number of arrows being shot at once
+    private int duration = 1100; // cooldown timer duration
+    private int upProg = 10; // what the progressBar increments by as it cools down
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bow);
 
-        numArrows = 1; // start out one arrow at a time
-
-        /**/ // set onered to be onegreen via visibility exploits
-
+        // general environment
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT); // locks screen to be "portrait"
 
-        // create images
-        bow = findViewById(R.id.Bow);
-        bow.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (progBow.getVisibility() != ProgressBar.VISIBLE) { // progress bar isn't cooling down
-                    /**/ // send gyro data to computer, along with numArrows
-                    barCool(); // activates cooldown timer
-                }
-            }
-        });
+        // create different bow images
+        bow = findViewById(R.id.Bow); bowtwo = findViewById(R.id.bowtwo); bowthree = findViewById(R.id.bowthree);
+        bowtwo.setVisibility(View.INVISIBLE); bowthree.setVisibility(View.INVISIBLE); // only bow with one arrow shows up
+        // set onClickListeners to execute the "fire();" command
+        bow.setOnClickListener(new OnClickListener() { @Override public void onClick(View v) { fire(); } });
+        bowtwo.setOnClickListener(new OnClickListener() { @Override public void onClick(View v) { fire(); } });
+        bowthree.setOnClickListener(new OnClickListener() { @Override public void onClick(View v) { fire(); } });
 
         // making the bow cooldown bar
         progBow = (ProgressBar)findViewById(R.id.bowProg);
@@ -74,11 +69,11 @@ public class Bow extends AppCompatActivity {
         progBow.setVisibility(ProgressBar.VISIBLE); // player can see the progBar
         /**/ // set different cases for one arrow, two arrows, etc
         if (numArrows == 2) { // two arrows being shot at once
-            duration = 1400; // duration of timer
-            upProg = 8; // what the progress bar increments by
+            duration = 1500; // duration of timer
+            upProg = 7; // what the progress bar increments by
         } else if (numArrows == 3) { // three arrows being shot at once
-            duration = 1700;
-            upProg = 6;
+            duration = 2100;
+            upProg = 5;
         } else { // just one arrow, default
             duration = 1100;
             upProg = 10;
@@ -99,21 +94,35 @@ public class Bow extends AppCompatActivity {
         numArrows = n;
     } /**/ // voice commands
 
+    private void fire() { // fires arrow
+        if (progBow.getVisibility() != ProgressBar.VISIBLE) { // progress bar isn't cooling down
+                    /**/ // send gyro data to computer, along with numArrows
+            barCool(); // activates cooldown timer
+        }
+    }
+
     private void changeArrow(int n) { // changes the number of arrows being shot at once
+        // changing the bow&arrow sprite
+        bow.setVisibility(View.INVISIBLE); bowtwo.setVisibility(View.INVISIBLE); bowthree.setVisibility(View.INVISIBLE);
         // sets all buttons to red
-        onered.setVisibility(View.VISIBLE);
-        twored.setVisibility(View.VISIBLE);
-        threered.setVisibility(View.VISIBLE);
+        onered.setVisibility(View.VISIBLE); onegreen.setVisibility(View.INVISIBLE);
+        twored.setVisibility(View.VISIBLE); twogreen.setVisibility(View.INVISIBLE);
+        threered.setVisibility(View.VISIBLE); threegreen.setVisibility(View.INVISIBLE);
         if (n == 3) { // 3x
             threegreen.setVisibility(View.VISIBLE);
             numArrows = 3;
+            bowthree.setVisibility(View.VISIBLE);
             /**/ // some other commands, to be sure... i.e. sending data to laptop
         } else if (n == 2) { // 2x
             twogreen.setVisibility(View.VISIBLE);
             numArrows = 2;
+            bowtwo.setVisibility(View.VISIBLE);
         } else { // only one arrow, base case
             onegreen.setVisibility(View.VISIBLE);
             numArrows = 1;
+            bow.setVisibility(View.VISIBLE);
         }
+
+        // gets correct bow&arrow sprite
     }
 }
