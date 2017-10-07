b84be3d5deefc9b9493cb3004776008768583259
aniemiec758 <32038608+aniemiec758@users.noreply.github.com>
2017-10-07 14:50:31 -0500

    Started messing around with the progress bar, first test

--- app/src/main/java/com/example/aniem/madhacks/Bow.java
+++ app/src/main/java/com/example/aniem/madhacks/Bow.java
@@ -1,29 +1,81 @@
 package com.example.aniem.madhacks;
 
 import android.content.pm.ActivityInfo;
+import android.os.CountDownTimer;
+import android.view.View.OnClickListener;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
 public class Bow extends AppCompatActivity {
 
+    private ProgressBar progBow;
     private View bow;
+    private Button button;
+    private int numArrows; // number of arrows being shot at once
+    private int duration; // cooldown timer duration
+    private int upProg; // what the progressBar increments by as it cools down
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bow);
 
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); // locks screen to be "portrait"
+        numArrows = 1; // start out one arrow at a time
+
+        /**/ // set onered to be onegreen via visibility exploits
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT); // locks screen to be "portrait"
 
         // create images
         bow = findViewById(R.id.Bow);
-        //bow.setTranslationY(Resources.getSystem().getDisplayMetrics().widthPixels / 2 - (bow.getWidth()/2)); // centers width
-        //bow.setTranslationX(Resources.getSystem().getDisplayMetrics().heightPixels / 2 - (bow.getHeight()/2)); // centers height
-        bow.setTranslationX(0);
-        bow.setTranslationY(0);
+        bow.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (progBow.getVisibility() != ProgressBar.VISIBLE) { // progress bar isn't cooling down
+                    /**/ // send gyro data to computer, along with numArrows
+                    barCool(); // activates cooldown timer
+                }
+            }
+        });
+
+        // making the bow cooldown bar
+        progBow = (ProgressBar)findViewById(R.id.bowProg);
+        progBow.setVisibility(ProgressBar.INVISIBLE); // makes it invisible at first
+        progBow.setProgress(0);
     }
+
+    private void barCool() {
+        progBow.setVisibility(ProgressBar.VISIBLE); // player can see the progBar
+        /**/ // set different cases for one arrow, two arrows, etc
+        if (numArrows == 2) { // two arrows being shot at once
+            duration = 2600; // duration of timer
+            upProg = 4; // what the progress bar increments by
+        } else if (numArrows == 3) { // three arrows being shot at once
+            duration = 3100;
+            upProg = 3;
+        } else { // just one arrow, default
+            duration = 2100;
+            upProg = 5;
+        }
+
+        new CountDownTimer(duration, 100) { // filling up the bar until completion
+            @Override
+            public void onTick(long millisUntilFinished) { progBow.setProgress((progBow.getProgress()+upProg)); } // fills bar
+            @Override
+            public void onFinish() { // when it's filled
+                progBow.setVisibility(ProgressBar.INVISIBLE); // goes back to being invisible
+                progBow.setProgress(0); // resets bar
+            }
+        }.start();
+    }
+
+    private void setNumArrows(int n) { // changes it to be one, two, or three arrows in the quiver
+        numArrows = n;
+    } /**/ // voice commands
 }
