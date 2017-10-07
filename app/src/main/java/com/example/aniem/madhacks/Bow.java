53950e53b7fe08e11a561117957077342b0c0c56
aniemiec758 <32038608+aniemiec758@users.noreply.github.com>
2017-10-07 13:19:57 -0500

    Initial commit

---
+++ app/src/main/java/com/example/aniem/madhacks/Bow.java
@@ -0,0 +1,27 @@
+package com.example.aniem.madhacks;
+
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Bow extends AppCompatActivity {
+
+    private View bow;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bow);
+
+        // create images
+        bow = (View)findViewById(R.id.Bow);
+        //bow.setTranslationY(Resources.getSystem().getDisplayMetrics().widthPixels / 2 - (bow.getWidth()/2)); // centers width
+        //bow.setTranslationX(Resources.getSystem().getDisplayMetrics().heightPixels / 2 - (bow.getHeight()/2)); // centers height
+        bow.setTranslationX(bow.getWidth()/2);
+        bow.setTranslationX(bow.getWidth()/2);
+        bow.setTranslationY(200);
+    }
+}
